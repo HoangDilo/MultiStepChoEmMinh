a10b4b9109e180113b121131a6ed70137305e35a
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2024-02-25 20:10:57 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import MultiStep from '@/components/MultiStep.vue';
+</script>
+
+<template>
+  <div class="main-view">
+    <MultiStep />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MultiStep.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="main-container">
+    <div class="phone-container">
+      <div class="swipper-container">
+        <div class="swipper" ref="swipperRef">
+          <div class="sections">
+            <img
+              src="https://www.simoptions.com/wp/wp-content/uploads/2023/06/easy-to-use-1.svg"
+            />
+          </div>
+          <div class="sections">
+            <img
+              src="https://www.simoptions.com/wp/wp-content/uploads/2023/06/easy-to-use-2.svg"
+              class="section-img"
+            />
+          </div>
+          <div class="sections">
+            <img
+              src="https://www.simoptions.com/wp/wp-content/uploads/2023/06/easy-to-use-3.svg"
+              class="section-img"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="phone-wrapper"></div>
+    </div>
+    <div class="multi-step-container">
+      <div class="progress-bar">
+        <div class="passed-progress" ref="passedProgressBarRef"></div>
+      </div>
+      <div class="steps-container">
+        <div class="step-paragraph" @click="() => handleClickStep(0)">
+          concac
+        </div>
+        <div class="step-paragraph" @click="() => handleClickStep(1)">
+          concac
+        </div>
+        <div class="step-paragraph" @click="() => handleClickStep(2)">
+          concac
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch, onMounted } from "vue";
+const curretStep = ref<number>(0);
+const swipperRef = ref<HTMLDivElement | null>(null);
+const passedProgressBarRef = ref<HTMLDivElement | null>(null);
+
+const handleClickStep = (step: number) => {
+  curretStep.value = step;
+};
+
+watch(curretStep, () => {
+  if (swipperRef.value) {
+    swipperRef.value.style.transform = `translateY(${
+      -340 * curretStep.value
+    }px)`;
+  }
+  if(passedProgressBarRef.value) {
+    passedProgressBarRef.value.style.height = `${curretStep.value * 100 / 2}%`;
+  }
+  console.log(curretStep.value);
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./MultiStep.scss";
+</style>
